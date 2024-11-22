89806913252fe4d6387af7df376e3db04472d9a5
KHON Kimly <kimlyjr11@gmail.com>
2024-11-22 09:35:59 +0700

    addtocart

--- products/product-single.php
+++ products/product-single.php
@@ -5,8 +5,8 @@
 <?php
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
-
-    // Prepare a statement to prevent SQL injection
+	// data for singleProduct
+    // Prepare a statement to prevent SQL injection for the main product
     $stmt = $conn->prepare("SELECT * FROM products WHERE id = ?");
     
     // Bind the parameter
@@ -14,7 +14,7 @@ if (isset($_GET['id'])) {
 
     // Execute the statement
     $stmt->execute();
-
+    
     // Get the result
     $result = $stmt->get_result();
 
@@ -24,11 +24,57 @@ if (isset($_GET['id'])) {
     // Check if the product was found
     if ($singleProduct) {
         // You can now use $singleProduct to display details
+		//data for relateProducts
+        // Prepare a statement to fetch related products
+        $relatedStmt = $conn->prepare("SELECT * FROM products WHERE type = ? AND id != ?");
+        
+        // Bind the parameters
+        $relatedStmt->bind_param("si", $singleProduct->type, $singleProduct->id); // Assuming type is a string and id is an integer
+
+        // Execute the statement
+        $relatedStmt->execute();
+        
+        // Get the result for related products
+        $relatedResult = $relatedStmt->get_result();
+
+        // Fetch all related products as an array of objects
+        $allRelatedProducts = [];
+        while ($relatedProduct = $relatedResult->fetch_object()) {
+            $allRelatedProducts[] = $relatedProduct;
+        }
+
+        // Now you can display related products or perform other actions with $allRelatedProducts
     } else {
         echo "<p>Product not found.</p>";
     }
+
+
+	// Add to cart
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $image = $_POST['image'];
+    $price = $_POST['price'];
+    $description = $_POST['description'];
+    $quantity = $_POST['quantity'];    
+    $user_id = $_SESSION['user_id'];
+
+    // Prepare the statement
+    $insert_cart = $conn->prepare("INSERT INTO cart (name, image, price, description, quantity, user_id) VALUES (?, ?, ?, ?, ?, ?)");
+
+    // Bind the parameters
+    $insert_cart->bind_param("ssdsii", $name, $image, $price, $description, $quantity, $user_id); 
+    // Assuming price is a double (d), and quantity and user_id are integers (i)
+
+    // Execute the statement
+    if ($insert_cart->execute()) {
+        echo "<p>Item added to cart successfully!</p>";
+    } else {
+        echo "<p>Error adding item to cart: " . $conn->error . "</p>";
+    }
+}
+
 } else {
-    echo "<p>No product ID specified.</p>";
+    echo "<p>No product ID specified.</p>";	
 }
 ?>
     <section class="home-slider owl-carousel">
@@ -56,7 +102,7 @@ if (isset($_GET['id'])) {
     			</div>
     			<div class="col-lg-6 product-details pl-md-5 ftco-animate">
     				<h3><?php echo $singleProduct->name; ?></h3>
-    				<p class="price"><span>$<?php echo $singleProduct->name; ?></span></p>
+    				<p class="price"><span>$<?php echo $singleProduct->price; ?></span></p>
     				<p>
 					<?php echo $singleProduct->description; ?>
 					</p>
@@ -78,18 +124,26 @@ if (isset($_GET['id'])) {
 					<div class="input-group col-md-6 d-flex mb-3">
 	             	<span class="input-group-btn mr-2">
 	                	<button type="button" class="quantity-left-minus btn"  data-type="minus" data-field="">
-	                   <i class="icon-minus"></i>
+	                   		<i class="icon-minus"></i>
 	                	</button>
-	            		</span>
+	            	</span>
+				<form method="POST" action="product-single.php?id=<?php echo $id; ?>">
+
 	             	<input type="text" id="quantity" name="quantity" class="form-control input-number" value="1" min="1" max="100">
 	             	<span class="input-group-btn ml-2">
-	                	<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
-	                     <i class="icon-plus"></i>
-	                 </button>
+	                		<button type="button" class="quantity-right-plus btn" data-type="plus" data-field="">
+	                     	<i class="icon-plus"></i>
+	                 	</button>
 	             	</span>
-	          	</div>
-          	</div>
-          	<p><a href="cart.html" class="btn btn-primary py-3 px-5">Add to Cart</a></p>
+	          		</div>
+          		</div>
+			
+				<input name="name" value="<?php echo $singleProduct->name; ?>" type="text">
+				<input name="image" value="<?php echo $singleProduct->image; ?>" type="text">
+				<input name="price" value="<?php echo $singleProduct->price; ?>" type="text">
+				<input name="description" value="<?php echo $singleProduct->description; ?>" type="text">
+          		<p><button name="submit" type="submit" class="btn btn-primary py-3 px-5">Add to Cart</button></p>
+			</form>	
     			</div>
     		</div>
     	</div>
@@ -105,50 +159,21 @@ if (isset($_GET['id'])) {
           </div>
         </div>
         <div class="row">
+			<?php foreach($allRelatedProducts as $allRelatedProduct) : ?>
         	<div class="col-md-3">
         		<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url(images/menu-1.jpg);"></a>
+    					<a href="<?php echo APPURL; ?>/products/product-single.php?id=<?php echo $allRelatedProduct->id; ?>" class="img" style="background-image: url(<?php echo APPURL; ?>/images/<?php echo $allRelatedProduct->image; ?>);"></a>
     					<div class="text text-center pt-4">
-    						<h3><a href="#">Coffee Capuccino</a></h3>
-    						<p>A small river named Duden flows by their place and supplies</p>
-    						<p class="price"><span>$5.90</span></p>
-    						<p><a href="#" class="btn btn-primary btn-outline-primary">Add to Cart</a></p>
+    						<h3><a href="<?php echo APPURL; ?>/products/product-single.php?id=<?php echo $allRelatedProduct->id; ?>"><?php echo $allRelatedProduct->name; ?></a></h3>
+    						<p>
+							<?php echo $allRelatedProduct->description; ?>
+							</p>
+    						<p class="price"><span>$<?php echo $allRelatedProduct->price; ?></span></p>
+    						<p><a href="<?php echo APPURL; ?>/products/product-single.php?id=<?php echo $allRelatedProduct->id; ?>" class="btn btn-primary btn-outline-primary">Show</a></p>
     					</div>
     				</div>
-        	</div>
-        	<div class="col-md-3">
-        		<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url(images/menu-2.jpg);"></a>
-    					<div class="text text-center pt-4">
-    						<h3><a href="#">Coffee Capuccino</a></h3>
-    						<p>A small river named Duden flows by their place and supplies</p>
-    						<p class="price"><span>$5.90</span></p>
-    						<p><a href="#" class="btn btn-primary btn-outline-primary">Add to Cart</a></p>
-    					</div>
-    				</div>
-        	</div>
-        	<div class="col-md-3">
-        		<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url(images/menu-3.jpg);"></a>
-    					<div class="text text-center pt-4">
-    						<h3><a href="#">Coffee Capuccino</a></h3>
-    						<p>A small river named Duden flows by their place and supplies</p>
-    						<p class="price"><span>$5.90</span></p>
-    						<p><a href="#" class="btn btn-primary btn-outline-primary">Add to Cart</a></p>
-    					</div>
-    				</div>
-        	</div>
-        	<div class="col-md-3">
-        		<div class="menu-entry">
-    					<a href="#" class="img" style="background-image: url(images/menu-4.jpg);"></a>
-    					<div class="text text-center pt-4">
-    						<h3><a href="#">Coffee Capuccino</a></h3>
-    						<p>A small river named Duden flows by their place and supplies</p>
-    						<p class="price"><span>$5.90</span></p>
-    						<p><a href="#" class="btn btn-primary btn-outline-primary">Add to Cart</a></p>
-    					</div>
-    				</div>
-        	</div>
+			</div>
+			<?php endforeach; ?>
         </div>
     	</div>
     </section>
